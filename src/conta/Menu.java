7ac3909004ebc07e773698ba05c09e58fc5a86ef
eliseflaneuse <eliseflaneuse@gmail.com>
2023-06-05 14:56:09 -0300

    Parte 2 do projeto conta bancaria - heranças

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,22 +1,49 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		
+
+		// Teste da Classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+
+		// Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+
 		Scanner leia = new Scanner(System.in);
-		
+
 		int option;
-		
+
 		do {
 			System.out.println(Cores.TEXT_GREEN_BOLD_BRIGHT);
 			System.out.println("\t _________________________________________");
 			System.out.println("\t|                                         |");
-			System.out.println("\t|        " + Cores.TEXT_WHITE_BOLD_BRIGHT + "      Banco da Elise      " + Cores.TEXT_RESET + Cores.TEXT_GREEN_BOLD_BRIGHT + "       |");
+			System.out.println("\t|        " + Cores.TEXT_WHITE_BOLD_BRIGHT + "      Banco da Elise      "
+					+ Cores.TEXT_RESET + Cores.TEXT_GREEN_BOLD_BRIGHT + "       |");
 			System.out.println("\t|_________________________________________|");
 			System.out.println("\t|                                         |");
 			System.out.println("\t|   1 - Criar Conta                       |");
@@ -30,24 +57,27 @@ public class Menu {
 			System.out.println("\t|   9 - Sair                              |");
 			System.out.println("\t|_________________________________________|");
 			System.out.println("\t|                                         |");
-			System.out.println("\t|    " + Cores.TEXT_WHITE_BRIGHT + "   Entre com a opção desejada:   " + Cores.TEXT_RESET + Cores.TEXT_GREEN_BOLD_BRIGHT + "    |");
+			System.out.println("\t|    " + Cores.TEXT_WHITE_BRIGHT + "   Entre com a opção desejada:   "
+					+ Cores.TEXT_RESET + Cores.TEXT_GREEN_BOLD_BRIGHT + "    |");
 			System.out.println("\t|_________________________________________|" + Cores.TEXT_RESET);
-			
-			option = leia.nextInt();
-			leia.nextLine(); //   utilizada para limpar o buffer de entrada
 
-			switch(option) {
+			option = leia.nextInt();
+			leia.nextLine(); // utilizada para limpar o buffer de entrada
+
+			switch (option) {
 			case 1:
-				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT  + "\n\t\t\tCriar Conta \n\n" + Cores.TEXT_RESET);
+				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\n\t\t\tCriar Conta \n\n" + Cores.TEXT_RESET);
 				break;
 			case 2:
-				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT  + "\t\tListar todas as Contas\n\n" + Cores.TEXT_RESET);
+				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tListar todas as Contas\n\n" + Cores.TEXT_RESET);
 				break;
 			case 3:
-				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tConsultar dados da Conta - por número\n\n" + Cores.TEXT_RESET);
+				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tConsultar dados da Conta - por número\n\n"
+						+ Cores.TEXT_RESET);
 				break;
 			case 4:
-				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tAtualizar dados da Conta\n\n" + Cores.TEXT_RESET);
+				System.out
+						.println(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tAtualizar dados da Conta\n\n" + Cores.TEXT_RESET);
 				break;
 			case 5:
 				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tApagar a Conta\n\n" + Cores.TEXT_RESET);
@@ -59,17 +89,18 @@ public class Menu {
 				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tDepósito\n\n" + Cores.TEXT_RESET);
 				break;
 			case 8:
-				System.out.println(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tTransferência entre Contas\n\n" + Cores.TEXT_RESET);
+				System.out.println(
+						Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tTransferência entre Contas\n\n" + Cores.TEXT_RESET);
 				break;
 			case 9:
 				System.out.printf(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\t\tPrograma finalizado." + Cores.TEXT_RESET);
 				break;
 			default:
-				System.out.printf(Cores.TEXT_YELLOW_BOLD_BRIGHT + "\tOpção inválida. Tente novamente." + Cores.TEXT_RESET);
-			}			
+				System.out.printf(
+						Cores.TEXT_YELLOW_BOLD_BRIGHT + "\tOpção inválida. Tente novamente." + Cores.TEXT_RESET);
+			}
 		} while (option != 9);
-		
-		
-		}
+
+	}
 
 }
